82304b43a07aa3cca48ad96ed33925ce2322330b
Muhammad Sultan <muhammad.sultan96@gmail.com>
2019-05-22 19:52:40 -0400

    new project

---
+++ src/main/java/com/eyeme/EyemeApplication.java
@@ -0,0 +1,13 @@
+package com.eyeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyemeApplication.class, args);
+	}
+
+}
